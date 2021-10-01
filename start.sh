2a374a0c694d2fddc17279596aee0092bb1c1918
Sergio <43149357+serhio-k@users.noreply.github.com>
2021-10-01 16:47:42 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+curl -LO https://raw.githubusercontent.com/epmc-tdm/deploy/main/docker-compose.yml && \
+mkdir rabbitconf
+
+if test -f ".env"; then
+export $(grep -v '^#' .env | xargs -0)
+fi
+
+echo "default_user=${TDM_RABBIT_MQ_USER:-tdm}">./rabbitconf/rabbitmq.conf
+echo -n "default_passwsord=${TDM_RABBIT_MQ_PWD:-pa55w0rd}">>./rabbitconf/rabbitmq.conf
