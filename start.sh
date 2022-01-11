f4a01c1b78cb28f5b116719d5ad139586c9ba5fd
epmc-tdm <79379446+epmc-tdm@users.noreply.github.com>
2022-01-11 23:47:22 +0300

    Fixed issues with bash location
    + Added check on folder rabbitconf exists
    + started docker-compose

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
-#!bin/bash
-curl -LO https://raw.githubusercontent.com/epmc-tdm/deploy/main/docker-compose.yml && \
-mkdir rabbitconf
+#!/bin/bash
+curl -LO https://raw.githubusercontent.com/epmc-tdm/deploy/main/docker-compose.yml
+if [ ! -d "./rabbitconf" ]; then 
+  mkdir rabbitconf
+fi
 
 if test -f ".env"; then
-export $(grep -v '^#' .env | xargs -0)
+  export $(grep -v '^#' .env | xargs -0)
 fi
 
 echo "default_user=${TDM_RABBIT_MQ_USER:-tdm}">./rabbitconf/rabbitmq.conf
 echo -n "default_pass=${TDM_RABBIT_MQ_PASSWORD:-pa55w0rd}">>./rabbitconf/rabbitmq.conf
+
+docker-compose up $@
